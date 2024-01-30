e65d7265bf6b4e7ce8f68d20758d03d0b25ec1f9
santiifm <flecha.mrc@hotmail.com>
2024-01-30 00:51:26 -0300

    add automatic CreatedAt field fill

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -13,5 +13,7 @@ namespace league_inferno_api.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
+
+        public BaseEntity() => this.CreatedAt = DateTime.UtcNow;
     }
 }
