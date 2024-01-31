1b09de2a61f2d166cb839ddabffc66af006f9011
santiifm <flecha.mrc@hotmail.com>
2024-01-31 00:48:49 -0300

    add new methods

--- Repository/ChampionsRepository.cs
+++ Repository/ChampionsRepository.cs
@@ -3,6 +3,7 @@ using System.Threading.Tasks;
 using league_inferno_api.Data;
 using league_inferno_api.DTOs;
 using league_inferno_api.Interfaces;
+using league_inferno_api.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace league_inferno_api.Repository
@@ -13,9 +14,7 @@ namespace league_inferno_api.Repository
 
         public async Task<IEnumerable<ChampionDTO>> GetChampionsAsync()
         {
-            var champions = await _context.Champions
-                                          .Include(c => c.Abilities)
-                                          .ToListAsync();
+            var champions = await _context.Champions.ToListAsync();
 
             if (champions == null)
             {
@@ -44,8 +43,53 @@ namespace league_inferno_api.Repository
             {
                 Id = champion.Id,
                 Name = champion.Name,
-                Description = champion.Description
+                Description = champion.Description,
+                Abilities = champion.Abilities?.Select(ability => new AbilityDTO
+                {
+                    Id = ability.Id,
+                    Name = ability.Name,
+                    Type = ability.Type,
+                    Description = ability.Description
+                })
             };
+        }
+            
+        public async Task<ChampionDTO> AddChampionAsync(BasicChampionDTO champion)
+        {
+            var champ = new Champion
+            {
+                Name = champion.Name,
+                Description = champion.Description,
+            };
+
+            try
+            {
+                await _context.Champions.AddAsync(champ);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                throw new Exception($"A problem ocurred during the DB transaction");
+            }
+
+            return new ChampionDTO
+            {
+                Id = champ.Id,
+                Name = champ.Name,
+                Description = champ.Description
+            };
+        }
+
+        public async Task DeleteChampionAsync(int championId)
+        {
+            var champion = await _context.Champions.FirstOrDefaultAsync(c => c.Id == championId); 
+
+            if (champion == null)
+            {
+                throw new Exception($"A champion with that Id doesn't exist.");
+            }
+            _context.Champions.Remove(champion);
+            await _context.SaveChangesAsync();
         }
     }
 }
