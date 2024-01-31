ee79353163020e19385f522a1ef99acfce91f13e
santiifm <flecha.mrc@hotmail.com>
2024-01-31 01:30:25 -0300

    removed unnecesary setters

--- Models/Role.cs
+++ Models/Role.cs
@@ -2,7 +2,7 @@ namespace league_inferno_api.Models
 {
     public static class Role
     {
-        public static string Admin { get; set; } = "Admin";
-        public static string User { get; set; } = "User";
+        public static string Admin { get; } = "Admin";
+        public static string User { get; } = "User";
     }
 }
