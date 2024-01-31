12524634ce71ba309406a0d332712f0f5dfe35bb
santiifm <flecha.mrc@hotmail.com>
2024-01-31 00:48:12 -0300

    remove annotations and change role

--- Models/User.cs
+++ Models/User.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 
 namespace league_inferno_api.Models
@@ -6,12 +5,9 @@ namespace league_inferno_api.Models
     [Index(nameof(Username), IsUnique = true)]
     public class User : BaseEntity
     {
-        [Required]
         public required string Username { get; set; }
-        [Required]
-        public required string Password { get; set; }
-        public int? RoleId { get; set; }
-        public Role? Role { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string Role { get; set; }
         public ICollection<Post>? Posts { get; set; }
         public ICollection<Comment>? Comments { get; set; }
     }
